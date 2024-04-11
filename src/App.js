7116ad82e30ec84c05b2aa836471151d1d882493
TilteD24 <21bcs080@iiitdmj.ac.in>
2024-04-11 22:09:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginComponent from "./components/Login/login";
+import Header from "./components/header";
+import Dashboard from "./components/Dashboard/dashboard";
+import Temperature from "./components/Dashboard/tempChart";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Ph from "./components/Dashboard/pHchart";
+import TDS from "./components/Dashboard/tdsChart";
+import Turbidity from "./components/Dashboard/turbidity";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [isAuthenticated, setAuthenticated] = useState(
+    localStorage.getItem("isAuthenticated") === "true"
   );
-}
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<LoginComponent setAuthenticated={setAuthenticated} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<Dashboard isAuthenticated={isAuthenticated} />}
+        />
+        <Route path="/dashboard/tempChart" element={<Temperature />} />
+        <Route path="/dashboard/pHChart" element={<Ph />} />
+        <Route path="/dashboard/tdsChart" element={<TDS />} />
+        <Route path="/dashboard/turbidityChart" element={<Turbidity />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div
+      style={{
+        height: "80px",
+        padding: "5px",
+        // boxShadow: "5px 10px 8px",
+        zIndex: "2",
+      }}
+    >
+      <h1 style={{ paddingLeft: "10px" }}>Aquaculture</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
