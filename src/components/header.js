ff437cc29610103b7cb87e326bfc0f9c9205b1f3
Abhikumar Gupta <abhi873gupta@gmail.com>
2024-04-15 00:04:24 +0530

    added some animations

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,17 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div
-      style={{
-        height: "80px",
-        padding: "5px",
-        // boxShadow: "5px 10px 8px",
-        zIndex: "2",
-      }}
+    style={{
+      height: "80px",
+      padding: "5px",
+      // boxShadow: "5px 10px 8px",
+      zIndex: "2",
+      backgroundImage: "linear-gradient(0deg, #182848, #2980b9)"
+    }}
+    
     >
-      <h1 style={{ paddingLeft: "10px" }}>Aquaculture</h1>
+      <h1 style={{ paddingLeft: "10px", color : "white"}}>Aquaculture</h1>
+
     </div>
   );
 };
